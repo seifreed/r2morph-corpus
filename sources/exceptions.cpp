5b715fb1fedf149ac1a79bcce093d0e8d221c047
Marc R. <mriverolopez@gmail.com>
2026-08-29 09:18:06 +0200

    Exercise unwind-safe exception corpus path

--- sources/exceptions.cpp
+++ sources/exceptions.cpp
@@ -7,6 +7,10 @@ static int recurse(int value) {
     return recurse(value - 1) + recurse(value - 2);
 }
 
+__attribute__((noinline)) static int safe_arithmetic(int value) {
+    return value * 3 + 1;
+}
+
 static int checked(int value) {
     if (value < 0) throw std::runtime_error("negative");
     return value * 3 + recurse(value % 5);
@@ -17,7 +21,7 @@ int main(int argc, char **argv) {
         int input = argc > 1 ? std::atoi(argv[1]) : 0;
         int values[] = {1 + input, 4, 7};
         int *cursor = values;
-        int total = 0;
+        int total = safe_arithmetic(input);
         for (int index = 0; index < 3; ++index) total += checked(cursor[index]);
         std::cout << total << "\n";
         return total & 0x7f;
